a31965bb29ac0cb7f2c4cc798a9fdffd266572b2
hossein-karami <hsn.karami@yahoo.com>
2020-10-13 22:51:23 +0200

    creating repository for practicing ML

---
+++ keras/practice_keras.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
